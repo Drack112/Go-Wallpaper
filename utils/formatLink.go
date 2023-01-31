a538cab674b836d55894e761a461a7e336c327a3
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-31 13:27:47 -0300

    💣 feat: remove lowercase string on request

--- utils/formatLink.go
+++ utils/formatLink.go
@@ -6,8 +6,7 @@ import (
 )
 
 func FormatLink(category string) string {
-    lowerCategory := strings.ToLower(category)
-    formatCategory := strings.ReplaceAll(lowerCategory, " ", "%20")
+    formatCategory := strings.ReplaceAll(category, " ", "+")
     link := fmt.Sprintf("https://www.wallpaperflare.com/search?wallpaper=%s", formatCategory)
 
     return link
--- utils/mobileSupport.go
+++ utils/mobileSupport.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func VerifyMobileSupport(link string, mobileConfirm bool) string {
     if mobileConfirm {
-        return fmt.Sprintf("%s&%s", link, "&mobile=ok")
+        return fmt.Sprintf("%s&%s", link, "mobile=ok")
     } else {
         return link
     }
