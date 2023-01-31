37fac350e840505eb3d4729659a1745d61d8796a
João Vitor <61944386+Drack112@users.noreply.github.com>
2023-01-31 12:47:06 -0300

    🤸‍♀ feat: add a method to lowercase the string of category

--- utils/formatLink.go
+++ utils/formatLink.go
@@ -6,7 +6,8 @@ import (
 )
 
 func FormatLink(category string) string {
-    formatCategory := strings.ReplaceAll(category, " ", "%20")
+    lowerCategory := strings.ToLower(category)
+    formatCategory := strings.ReplaceAll(lowerCategory, " ", "%20")
     link := fmt.Sprintf("https://www.wallpaperflare.com/search?wallpaper=%s", formatCategory)
 
     return link
